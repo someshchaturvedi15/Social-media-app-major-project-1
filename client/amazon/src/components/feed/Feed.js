5f3fd9d4c9b4fca178c24e9443affd5e9de7d6c4
Aryaman Raj Tiwari <i.aryantiwari@gmail.com>
2023-09-08 15:58:35 +0530

    Post API issue fetched successfully

--- client/amazon/src/components/feed/Feed.js
+++ client/amazon/src/components/feed/Feed.js
@@ -1,38 +1,43 @@
-import React, { useEffect } from 'react';
-import './Feed.scss';
-import Post from '../post/Post';
-import Follower from '../follower/Follower';
-import {useSelector, useDispatch} from 'react-redux';
-import { getFeedData } from '../../redux/slices/feedSlice';
+import React, { useEffect } from "react";
+import "./Feed.scss";
+import Post from "../post/Post";
+import Follower from "../follower/Follower";
+import { useSelector, useDispatch } from "react-redux";
+import { getFeedData } from "../../redux/slices/feedSlice";
 
 function Feed() {
-
   const dispatch = useDispatch();
-  const feedData = useSelector(state => state.feedDataReducer.feedData)
+  const feedData = useSelector((state) => state.feedDataReducer.feedData);
 
   useEffect(() => {
     dispatch(getFeedData());
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
-    <div className='Feed'>
-        <div className="container">
-<div className="left-part">
-{feedData?.posts?.map(post => <Post key={post._id} post={post} />)}
-</div>
-<div className="right-part">
-<div className="following">
-<h3 className='title'>You are Following</h3>
-{feedData?.followings?.map(user => <Follower key={user._id} user={user} />)}
-</div>
-<div className="suggestions">
-<h3 className='title'>Suggestions for you</h3>
-{feedData?.suggestions?.map(user => <Follower key={user._id} user={user} />)}   
-</div>
-</div>
-</div> 
-</div>
-  )
+    <div className="Feed">
+      <div className="container">
+        <div className="left-part">
+          {feedData?.posts?.map(post =>
+            <Post key={post._id} post={post} />
+          )}
+        </div>
+        <div className="right-part">
+          <div className="following">
+            <h3 className="title">You are Following</h3>
+            {feedData?.followings?.map((user) => (
+              <Follower key={user._id} user={user} />
+            ))}
+          </div>
+          <div className="suggestions">
+            <h3 className="title">Suggestions for you</h3>
+            {feedData?.suggestions?.map((user) => (
+              <Follower key={user._id} user={user} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
